2078ea5a9157bbaafaee873be8f7ff2808e7ab07
Richard Otto <richardsoeuzin@gmail.com>
2020-07-05 22:52:57 -0300

    feat(server): fixed CORS and mocked auth routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -8,9 +9,14 @@ if (process.env.NODE_ENV === "production")
   console.log("Oh noes, we shouldn't be in prod yet D:");
 
 app.use(morgan('dev'));
+app.use(cors({ origin: "http://localhost:3000" }));
 
-app.get("/", (req, res) => {
+const defaultResponse = (req, res) => {
   res.status(200).send("Hol' up, server is still to be implemented");
-});
+}
+
+app.get("/", defaultResponse);
+app.post("/auth/signup", defaultResponse);
+app.post("/auth/login", defaultResponse);
 
 app.listen(port, () => console.log(`Server is up on port ${port}!`));
