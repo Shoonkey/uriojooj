964e0c081c5a2990a31c88173e8c0e987d9caf1b
Richard Otto <richardsoeuzin@gmail.com>
2020-07-13 18:24:47 -0300

    feat(client): auth page is now more responsive

--- client/src/pages/Auth/styles.js
+++ client/src/pages/Auth/styles.js
@@ -4,31 +4,35 @@ import { Container as Input } from '../../components/Input/styles';
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 
   background: var(--dark-grey);
   color: var(--light-grey);
-  
+
   .logo {
     font-family: Saira;
     font-size: 3em;
     text-align: right;
-    margin-right: 2em;
 
     span {
-
-      display: block;
+      
       position: relative;
 
+      &:not(.highlighted){
+        top: -0.85em;
+        padding: 0 4px;
+      }
+
       &.highlighted {
+        display: block;
         font-family: Righteous;
         font-size: 3em;
         color: var(--primary);
       }
 
-      &:nth-child(2){ top: -0.85em; }
-      &:nth-child(3){ top: -1.50em; }
+      
 
     }
   }
@@ -37,20 +41,18 @@ export const Container = styled.div`
     display: flex;
     align-items: center;
 
-    width: 60vh;
-    height: 60vh;
+    width: min(400px, 90%);
+
     background: #222222;
 
-    form, .no-type-defined { padding-left: 4em; }
+    form, .no-type-defined { margin: 3em auto; }
 
     .no-type-defined button { display: block; }
 
     form {
-      /* width: 100%; */
-      padding-left: 4em;
 
       button { margin-top: 1em; }
-      ${Input}, .btn-group, .extra { width: max(200px, 50%); }
+      &, ${Input}, .btn-group, .extra { width: max(200px, 50%); }
 
       .btn-group {
         display: flex;
@@ -70,5 +72,36 @@ export const Container = styled.div`
 
     }
 
+  }
+
+  @media screen and (min-width: 721px){
+    flex-direction: row;
+    padding: 0 1em;
+
+    .logo { 
+      margin-right: 2em; 
+      span { 
+        display: block; 
+
+        /* compensation for the "jooj" so that it aligns properly when stacked */
+        /* would be necessary if there were more lines, because of the way font is rendered */ 
+        &:nth-child(3){ top: -1.50em; }
+      }
+
+    }
+
+    .form-container {
+
+      width: 60vh;
+      height: 60vh;
+
+      form, .no-type-defined { 
+        width: 100%;
+        padding: 3em;
+        margin-left: 1rem;
+      }
+
+    }
+    
   }
 `;
