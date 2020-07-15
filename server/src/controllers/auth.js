d0b4b768b83a6818701ecd5b11bae14a9df175c4
Richard Otto <richardsoeuzin@gmail.com>
2020-07-14 22:45:33 -0300

    fix(server): now auth routes return token and user

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -10,7 +10,10 @@ export async function login({ nicknameOrEmail, password }){
   const prop = isEmail(nicknameOrEmail) ? "email" : "nickname";
   const where = { [prop]: nicknameOrEmail };
 
-  const user = await User.findOne({ where, attributes: ["id", "name", "password"] });
+  let user = await User.findOne({ 
+    where,
+    attributes: ["nickname", "name", "password"]
+  });
 
   if (!user)
     throw new Error(`User with ${prop} "${nicknameOrEmail}" not found`);
@@ -18,9 +21,13 @@ export async function login({ nicknameOrEmail, password }){
   if (!bcrypt.compareSync(password, user.password))
     throw new Error("Incorrect password");
   
+  user = user.toJSON();
   delete user.password;
 
-  return { token: jwt.sign({ user }, process.env.JWT_SECRET) };
+  return { 
+    token: jwt.sign({ user }, process.env.JWT_SECRET),
+    user
+  };
 
 }
 
@@ -41,14 +48,27 @@ export async function signup({ name, nickname, email, password }){
 
   try {
 
+    // The `returning` argument with an array of fields currently has no effect on Sequelize
+    // (https://github.com/sequelize/sequelize/issues/11904)
+    /*
+      const user = await User.create(
+        { name, nickname, email, password: hash }, 
+        { returning: ["id", "name" ] }
+      );
+    */
+
     const user = await User.create(
-      { name, nickname, email, password: hash }, 
-      { returning: ["id", "name" ] }
+      { name, nickname, email, password: hash },
+      { returning: true }
     );
 
-    return { token: jwt.sign({ user }, process.env.JWT_SECRET) };
+    return { 
+      token: jwt.sign({ user }, process.env.JWT_SECRET),
+      user: { name: user.name, nickname: user.nickname }
+    };
 
   } catch (e){
+    console.error(e);
     throw new Error("We weren't able to create your account.");
   }
 
