8499b0e4577617d4ea8cbda85e8034a8e1002c11
Richard Otto <richardsoeuzin@gmail.com>
2020-07-19 03:04:18 -0300

    feat(client/button): new 'discreet' theme

--- client/src/components/Button/styles.js
+++ client/src/components/Button/styles.js
@@ -4,13 +4,26 @@ import { Link } from 'react-router-dom';
 const styles = {
   primary: {
     bgColor: "var(--primary)",
-    color: "black"
+    color: "black",
+    hover: {
+      bgColor: "#F84C4C",
+      color: "black"
+    }
+  },
+  discreet: {
+    bgColor: "transparent",
+    color: "var(--light-grey)",
+    hover: {
+      bgColor: "transparent",
+      color: "grey"
+    }
   }
 }
 
 const rules = props => `
   font-family: Saira;
   font-weight: bold;
+  font-size: 1em;
   cursor: pointer;
   background-color: ${styles[props.theme].bgColor};
   color: ${styles[props.theme].color};
@@ -18,7 +31,8 @@ const rules = props => `
   border: none;
 
   &:hover {
-    background-color: #F84C4C;
+    background-color: ${styles[props.theme].hover.bgColor};
+    color: ${styles[props.theme].hover.color};
   }
 `;
 
